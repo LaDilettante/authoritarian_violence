ad206a28223ddc217579bea72fd106d13af10e3a
Anh Le <aql3@duke.edu>
2014-09-16 14:56:38 -0400

    fix new link location for source R file

--- ir.r
+++ ir.r
@@ -1,20 +1,21 @@
+rm(list=ls())
 ###
-Y<-dget("http://www.stat.washington.edu/hoff/Code/GBME/IR/Y")
+Y<-dget("http://www.stat.washington.edu/hoff/Code/hoff_2005_jasa/IR/Y")
+
 n<-dim(Y)[1]
 
-Xd<-dget("http://www.stat.washington.edu/hoff/Code/GBME/IR/X.dist") #distance
+Xd<-dget("http://www.stat.washington.edu/hoff/Code/hoff_2005_jasa/IR/X.dist") #distance
 Xd<-array(Xd,dim=c(n,n,1))/1000
 
-Xp<-dget("http://www.stat.washington.edu/hoff/Code/GBME/IR/X.pop") #pops
+Xp<-dget("http://www.stat.washington.edu/hoff/Code/hoff_2005_jasa/IR/X.pop") #pops
 Xp<-log(matrix( Xp,nrow=n,ncol=1))
 
 ###
 
-source("gbme.r")
-source("http://www.stat.washington.edu/hoff/Code/GBME/gbme.r")
+source("http://www.stat.washington.edu/hoff/Code/hoff_2005_jasa/gbme.r")
 
 ##
 gbme(Y=Y,Xd=Xd,Xs=Xp,Xr=Xp,fam="poisson",k=2,odens=50)                       
 
-source("http://www.stat.washington.edu/hoff/Code/GBME/gbme.postana.r")
+source("http://www.stat.washington.edu/hoff/Code/hoff_2005_jasa/gbme.postana.r")
  
