ec5344f1db2d8f418c07bf131a72c502a10888ca
Anh Le <aql3@duke.edu>
2014-10-21 14:39:28 -0400

    taking a look at transition years

--- R/0_clean.R
+++ R/0_clean.R
@@ -10,17 +10,15 @@ f_install_and_load(packs)
 load("./data/private/credentials.RData") # Load credentials
 db.my_tables = dbConnect(MySQL(), dbname='my_tables', host=credentials[["host"]],
                          user=credentials[["username"]], password=credentials[["password"]])
-qr_disgov = dbSendQuery(db.my_tables, "SELECT * FROM anh_dis_to_gov_count
-                                       WHERE target_country_democracy = 0")
+qr_disgov = dbSendQuery(db.my_tables, "SELECT * FROM anh_dis_to_gov_count")
 d_disgov_raw = fetch(qr_disgov, n=-1)
 d_disgov <- d_disgov_raw %>%
-  select(iso3c = target_country_ISOA3Code, year, 
+  select(iso3c = target_country_ISOA3Code, year,
          country = target_country_name,
          source_actor_name, source_actor_id, source_sector_name, 
          goldstein_avg, goldstein_sum, goldstein_pos_count, goldstein_neg_count)
 
-qr_disgovsect <- dbSendQuery(db.my_tables, "SELECT * FROM anh_dis_to_gov_count_sector
-                                           WHERE target_country_democracy = 0")
+qr_disgovsect <- dbSendQuery(db.my_tables, "SELECT * FROM anh_dis_to_gov_count_sector")
 d_disgovsect_raw <- fetch(qr_disgovsect, n=-1)
 d_disgovsect <- d_disgovsect_raw %>%
   select(iso3c = target_country_ISOA3Code, year, 
@@ -35,11 +33,13 @@ d_dpi_raw <- DpiGet(vars=c("liec"))
 d_dpi <- d_dpi_raw %>% 
   mutate(iso3c = countrycode(iso2c, origin="iso2c", destination="iso3c", warn=T)) %>%
   mutate(liec = ifelse(liec==-999.0, NA, liec)) %>%
-  mutate(legis_multi = ifelse(liec >=5, 1, 0)) %>%
+  mutate(liec5 = ifelse(liec >= 5, 1, 0),
+         liec6 = ifelse(liec >= 6, 1, 0),
+         liec7 = ifelse(liec >= 7, 1, 0)) %>%
   group_by(iso3c, year) %>%
   mutate(liec = mean(liec)) %>%
   filter(row_number() == 1) %>%
-  select(iso3c, year, country, liec, legis_multi)
+  select(iso3c, year, country, liec, liec5, liec6, liec7)
 
 # ---- Load WDI data ----
 # GDP constant 2005 dollar, GDP per cap constant 2011 dollars, mil exp %GDP, 
@@ -73,22 +73,26 @@ f_find_transition_point <- function(df) {
   return(df_democratic_transition)
 }
 # Find countries from 1991 to 2014 where transition happened
-d_democratic_transition <- ddply(d_dd[d_dd$year > 1990 , ], c("wdicode"), f_find_transition_point)
-d_democratic_transition <- d_democratic_transition %>%
-  mutate(iso3c = countrycode(cowcode, "cown", "iso3c")) %>%
+d_democratic_transition_year <- ddply(d_dd[d_dd$year > 1990 , ], c("cowcode"), f_find_transition_point)
+d_democratic_transition_year <- d_democratic_transition_year %>%
+  mutate(iso3c = countrycode(cowcode, "cown", "iso3c", warn=TRUE)) %>%
   select(iso3c, cowcode, ctryname, year, democracy, democracy_previous)
 
-d_tmp <- ddply(d_democratic_transition, c("iso3c", "year"),
-               function(df) data.frame(wanted_years = (df$year - 10):df$year))
-d_tmp2 <- merge(d_tmp, d_dpi, by.x=c("iso3c", "wanted_years"), by.y=c("iso3c", "year"))
-d_tmp3 <- ddply(d_tmp2, c("iso3c", "year"), summarize, mean_liec = mean(liec), mean_legis = mean(legis_multi))
-  
+d_before_transition <- ddply(d_democratic_transition_year, c("iso3c", "year"),
+               function(df) data.frame(year = (df$year - 9):df$year,
+                                       transition_year = rep(df$year, 10)))
+
 # ---- Merge data ----
 
 d_disgov_merged_full <- Reduce(function(...) merge(..., match=c("iso3c", "year"), all.x=T, sort=T),
                    list(d_disgov, d_dpi, d_wdi, d_geddes))
 d_disgovsect_merged_full <-  Reduce(function(...) merge(..., match=c("iso3c", "year"), all.x=T, sort=T),
                                     list(d_disgovsect, d_dpi, d_wdi, d_geddes))
+d_exp_before_trans_full <- Reduce(function(...) merge(..., match=c("iso3c", "year"), all.x=T, sort=T),
+  list(d_before_transition, d_dpi, d_wdi, d_geddes))
+
+
 
 # ---- Save cleaned data ----
-save(d_disgov_merged_full, d_disgovsect_merged_full, d_democratic_transition, file='./data/private/data_final.RData')
+save(d_disgov_merged_full, d_disgovsect_merged_full, d_exp_before_trans_full,
+     file='./data/private/data_final.RData')
--- R/violent_transition.R
+++ R/violent_transition.R
@@ -7,9 +7,24 @@ f_install_and_load(packs) ; rm(packs)
 # ---- Load data ----
 load('./data/private/data_final.RData')
 
-# I have a bunch of country years
-# Get the country and years 10 year before that
+d_exp_before_trans_full <- d_exp_before_trans_full %>%
+  filter(is.na(iso3c)==FALSE)
 
+d_transition <- ddply(d_exp_before_trans_full, c("iso3c", "transition_year"), 
+                      colwise(mean, .(liec, liec5, liec6, liec7,
+                                      gwf_military, gwf_personal,
+                                      gwf_party, gwf_monarchy), na.rm=TRUE))
+d_transition2 <- merge(d_transition, d_exp_before_trans_full[ , c("iso3c", "year", "gdp", "gdppc", "milexp")],
+                       by.x=c("iso3c", "transition_year"), by.y=c("iso3c", "year"))
 
+d_violence <- ddply(d_disgov_merged_full, c("iso3c", "year"), summarize,
+                    count = sum(goldstein_neg_count))
 
-merge(d_tmp, )
+d_transition_violence <- merge(d_transition2, d_violence, 
+                               by.x=c("iso3c", "transition_year"),
+                               by.y=c("iso3c", "year"))
+
+names(d_transition_violence)
+
+m_transition <- lm(count ~ liec5 + log(gdp) + gdppc + milexp, data=d_transition_violence)
+summary(m_transition)
