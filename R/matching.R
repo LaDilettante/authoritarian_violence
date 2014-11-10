6e6158cf508e9b055ea2d5d78d5e5c5bbb0e19ea
Anh Le <aql3@duke.edu>
2014-11-10 17:59:40 -0500

    working on matching

--- R/matching.R
+++ R/matching.R
@@ -8,7 +8,7 @@ rm(list=ls())
 # Load external functions
 source("./R/functions.R")
 # Load packages
-packs <- c("plyr", "dplyr", "ggplot2")
+packs <- c("arm", "plyr", "dplyr", "ggplot2")
 f_install_and_load(packs)
 
 # Load data
@@ -20,7 +20,10 @@ f_turn_country_into_panel <- function(df, t=5, idvar="country", timevar="year", 
   idx <- lapply(1:(n-t+1), FUN=`+`, 0:(t-1))
   # Create a list of short panels
   short_panels <- lapply(idx, function(i) 
-    data.frame(df[i, setdiff(names(df), "year")], year=0:4))
+    data.frame(df[i, setdiff(names(df), "year")], year=0:(t-1)))
+  # Add first difference to each short_panel
+  # short_panels <- lapply(short_panels, function(df)
+  #  mutate(df, goldstein_avg_growth=c(NA, diff(goldstein_avg) / goldstein_avg[-length(goldstein_avg)] * 100)))
   # Name that list
   names(short_panels) <- df$year[1:(n-4)]
   # Convert each short panel in the list from long to wide
@@ -28,7 +31,8 @@ f_turn_country_into_panel <- function(df, t=5, idvar="country", timevar="year", 
                                  idvar=idvar, timevar=timevar, drop=drop, direction="wide")
   # rbind and returns those widened panels
   res <- do.call(rbind, args=widened_short_panels)
-  res <- data.frame(start.year = rownames(res), res)
+  res <- data.frame(start.year = as.numeric(rownames(res)), 
+                    end.year = as.numeric(rownames(res)) + t - 1, res)
   return(res)
 }
 
@@ -40,29 +44,43 @@ d_long <- select(d_countrylevel, country, year, goldstein_avg, resource.pc,
 d_wide <- ddply(d_long, .(country),
              f_turn_country_into_panel, t=5, idvar="country", timevar="year")
 d_wide <- merge(d_wide, d_countrylevel[ , c("country", "year", "liec7")], 
-                     by.x=c("country", "start.year"), by.y=c("country", "year"))
-d_wide <- na.omit(d_wide)
+                     by.x=c("country", "end.year"), by.y=c("country", "year"))
+d_wide <- d_wide[ , as.logical(1 - grepl("(land|ethnic.polarization).[1-4]", names(d_wide)))]
+# d_wide <- select(d_wide, -goldstein_avg_growth.0)
 
-# Model propensity
-m_psfit <- glm(liec7 ~ ., data=select(d_wide, -country, -start.year), family=binomial(link="logit"))
+# ---- Model propensity (with cross validation) ----
+d_wide <- na.omit(d_wide)
+set.seed(1023)
+train <- sample(c(T, F), size=nrow(d_wide), replace=TRUE, prob=c(0.75, 0.25))
+
+m_pstrain <- glm(liec7 ~ .,
+               data=select(d_wide[train, ], -country, -start.year, -end.year), 
+               family=binomial(link="logit"))
+summary(m_pstrain)
+
+# Calculate propensity score and check predictive fit
+pscore_train <- predict(m_pstrain, newdata=d_wide[-train, ], type="response")
+predicted.liec7 <- ifelse(pscore_train >= 0.5, 1, 0)
+table(predicted.liec7, d_wide[-train, "liec7"])
+
+# ---- Model propensity (whole sample) ----
+
+m_psfit <- glm(liec7 ~ .,
+                 data=select(d_wide, -country, -start.year, -end.year), 
+                 family=binomial(link="logit"))
 summary(m_psfit)
 
-# Calculate propensity score
-pscore1 <- predict(m_psfit, type="link")
-pscore2 <- predict(m_psfit, type="response")
-
-# Check propensite score model fit
-predicted.liec7 <- ifelse(pscore2 >= 0.5, 1, 0)
-table(predicted.liec7, m_psfit$y)
-
 # Retrieve the matched sample (without replacement)
-matches <- f_matching(z=m_psfit$y, score=pscore1, replace=TRUE)
+pscore_fit <- predict(m_psfit, type="link")
+
+tmp <- cbind.data.frame(d_wide[, c("country", "start.year")], pscore_fit)
+
+
+matches <- matching(z=m_psfit$y, score=pscore_fit, replace=TRUE)
 d_wide_matched <- d_wide[matches$matched, ]
 
 # Show the matches
-matches$original <- as.numeric(rownames(matches))
-rownames(matches) <- NULL
-matches <- filter(matches, matched != 0)
+matches$original <- 1:length(matches$matched)
 
 tmp1 <- d_wide[matches$original, c("country", "start.year")]
 tmp2 <- d_wide[matches$matched, c("country", "start.year")]
